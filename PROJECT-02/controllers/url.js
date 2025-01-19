44304883b6c5cb8994bc783fdb0752fd02ead04f
Rabindranath Lodha <rabilodha789@gmail.com>
2025-01-19 23:52:48 +0530

    url shortner without authentication

--- PROJECT-02/controllers/url.js
+++ PROJECT-02/controllers/url.js
@@ -3,16 +3,32 @@ const URL = require('../models/url');
 
 async function handleGenerateNewShortUrl(req, res) {
     const body = req.body;
-    const shortID = nanoid(8);
-    if(!body.url) {
-        return res.status(400).json({ error: 'url is required' });
+
+    if (!body.url) {
+        return res.status(400).json({ error: 'URL is required' });
     }
-    await URL.create({ 
+
+    // Check if the URL already exists in the database
+    let existingEntry = await URL.findOne({ redirectURL: body.url });
+
+    if (existingEntry) {
+        // If URL exists, return the existing short ID
+        return res.render('home', {
+            id: existingEntry.shortID
+        });
+    }
+
+    // If URL does not exist, create a new short ID
+    const shortID = nanoid(8);
+    await URL.create({
         shortID: shortID,
         redirectURL: body.url,
         visitHistory: []
     });
-    return res.json({id: shortID});
+
+    return res.render('home', {
+        id: shortID
+    });
 }
 
 module.exports = {
--- PROJECT-02/index.js
+++ PROJECT-02/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const path = require('path');
 const urlRoute = require('./routes/url');
 const { connect } = require('./connection');
 const URL = require('./models/url');
+const staticRoute = require('./routes/staticRouter');
 
 const app = express();
 const PORT = 3000;
@@ -11,9 +13,21 @@ connect("mongodb://127.0.0.1:27017/short-url")
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Error connecting to MongoDB', err));
 
+app.set('view engine', 'ejs');
+app.set('views', path.resolve('./views'));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/url', urlRoute);
+app.use('/', staticRoute);
+
+app.get('/test', async (req, res) => {
+    const allUrls = await URL.find({});
+    return res.render('home', {
+        urls: allUrls,
+    })
+});
 
 app.get('/:shortID', async (req, res) => {
     const shortID = req.params.shortID;
@@ -27,9 +41,16 @@ app.get('/:shortID', async (req, res) => {
                     timestamp: Date.now(),
                 }
             }
-        }
+        },
+        { new: true } // This option makes sure to return the updated document
     );
+
+    if (!entry) {
+        return res.status(404).send('Short URL not found');
+    }
+
     res.redirect(entry.redirectURL);
 });
+
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
