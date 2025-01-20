596e2f973d26a58b44e682760b1c76c67a6d3aab
Rabindranath Lodha <rabilodha789@gmail.com>
2025-01-20 22:37:05 +0530

    stateless auth learned using jwt

--- PROJECT-02/service/auth.js
+++ PROJECT-02/service/auth.js
@@ -1,12 +1,23 @@
-const sessionIdToUserMap = new Map();
+const jwt = require('jsonwebtoken')
+const secret = "Rabindra@250704"
 
-function setUser(id, user){
-    sessionIdToUserMap.set(id, user);
+function setUser(user){
+    return jwt.sign({
+        _id: user._id,
+        email: user.email,
+    }, secret)
 }
 
-function getUser(id){
-    return sessionIdToUserMap.get(id);
+function getUser(token){
+    if(!token) return null;
+    try {
+        return jwt.verify(token, secret);
+    } catch (err) {
+        console.error('Invalid token', err);
+        return null;
+    }
 }
+
 
 module.exports = {
     setUser,
